d1d581bbb9166299aaff5f7867935e06d040d352
CinkaFox <leofoxm40@gmail.com>
2025-05-07 13:01:41 +0300

    - add: publish script

---
+++ publish.sh
@@ -0,0 +1,11 @@
+cd "$(dirname "$0")"
+
+dotnet publish -c Release -r win-x64 -o ./publish -p:IncludeNativeLibrariesForSelfExtract=true Nebula.UpdateResolver
+dotnet publish -c Release -r linux-x64 -o ./publish -p:IncludeNativeLibrariesForSelfExtract=true Nebula.UpdateResolver
+
+mv ./publish/Nebula.UpdateResolver.exe ./publish/NebulaLauncher.exe
+mv ./publish/Nebula.UpdateResolver ./publish/NebulaLauncher
+mv ./publish/Nebula.UpdateResolver.pdb ./publish/NebulaLauncher.pdb
+
+zip ./publish/NebulaLauncher_win64.zip ./publish/NebulaLauncher.exe
+zip ./publish/NebulaLauncher_linux64.zip ./publish/NebulaLauncher
